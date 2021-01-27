f536441e3df5bca2a267e79f6a6b6943c8637834
RSB <RSB@192.168.1.218>
2021-01-27 12:05:09 +0100

    .

---
+++ product-service/src/main/java/fon/master/nst/productservice/model/Product.java
@@ -0,0 +1,8 @@
+package fon.master.nst.productservice.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Product {
+
+}
---
+++ product-service/src/main/java/fon/master/nst/productservice/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package fon.master.nst.productservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fon.master.nst.productservice.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
