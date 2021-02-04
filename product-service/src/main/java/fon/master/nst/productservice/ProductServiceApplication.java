a3a512ee53ec861a6fea01f9e64b321a86e091d6
aleks <aleks@DESKTOP-VBAP95H>
2021-02-04 23:12:58 +0100

    .

--- product-service/src/main/java/fon/master/nst/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/fon/master/nst/productservice/ProductServiceApplication.java
@@ -3,9 +3,10 @@ package fon.master.nst.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
